962a8d3c5195b9a15049024737cf2012a108a0d3
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-06 21:11:12 +0400

    Centralized component exports in index file for streamlined usage

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navigation } from "./navigation/Navigation";
+export { default as Home } from "./home/Home";
+export { default as Services } from "./services/Services";
+export { default as Tours } from "./tours/Tours";
