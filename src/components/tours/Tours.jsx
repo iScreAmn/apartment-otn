ed80159db87f23668d15e063a6059a38c1d5ab79
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-06 21:17:10 +0400

    Linked Tours.jsx to the component for proper styling

---
+++ src/components/tours/Tours.jsx
@@ -0,0 +1,43 @@
+import "./Tours.css"
+
+const Tours = () => {
+  return (
+    <section className="tours">
+    <div className="container">
+      <h2 className="services-title">Чем заняться в Грузии <br/><span>в любой сезон:</span></h2>
+      <div className="toggle__wrapper">
+        <div className="tour-tab">Летний досуг</div>
+        <div className="tour-tab active">Зимний досуг</div>
+      </div>
+      <div className="tour__wrapper">
+        <div className="tour">
+          <h3 className="tour__title">Тбилиские бани</h3>
+          <p className="tour__subtitle">100$ до 2 часов парения</p>
+        </div>
+        <div className="tour">
+          <h3 className="tour__title">Тбилиские бани</h3>
+          <p className="tour__subtitle">100$ до 2 часов парения</p>
+        </div>
+        <div className="tour">
+          <h3 className="tour__title">Тбилиские бани</h3>
+          <p className="tour__subtitle">100$ до 2 часов парения</p>
+        </div>
+        <div className="tour">
+          <h3 className="tour__title">Тбилиские бани</h3>
+          <p className="tour__subtitle">100$ до 2 часов парения</p>
+        </div>
+        <div className="tour">
+          <h3 className="tour__title">Тбилиские бани</h3>
+          <p className="tour__subtitle">100$ до 2 часов парения</p>
+        </div>
+        <div className="tour">
+          <h3 className="tour__title">Тбилиские бани</h3>
+          <p className="tour__subtitle">100$ до 2 часов парения</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Tours
