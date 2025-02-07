111d289ced687f8f2fde8f2bcff00c248265e2ab
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-07 04:40:51 +0400

    Updated registration button with motion hover

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,6 +1,6 @@
-import { logo } from "../../assets/images"
-import "./Navigation.css"
-
+import { motion } from "framer-motion";
+import { logo } from "../../assets/images";
+import "./Navigation.css";
 
 const navigation = () => {
   return (
@@ -14,29 +14,42 @@ const navigation = () => {
           </div>
           <div className="nav__list">
             <ul>
-              <li><a href="#!">Главная</a></li>
-              <li><a href="#!">Услуги</a></li>
-              <li><a href="#!">Галерея</a></li>
-              <li><a href="#!">Контакты</a></li>
+              <li>
+                <a href="#!">Главная</a>
+              </li>
+              <li>
+                <a href="#!">Услуги</a>
+              </li>
+              <li>
+                <a href="#!">Галерея</a>
+              </li>
+              <li>
+                <a href="#!">Контакты</a>
+              </li>
             </ul>
           </div>
-          <a className="nav__btn" href="#!">Забронировать</a>
+          <motion.button
+            className="nav__btn"
+            href="#!"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            onHoverStart={() => console.log("hover started!")}
+          >
+            Регистрация
+          </motion.button>
           <button className="nav__toggle">
             <div className="menu-icon-wrapper">
               <div className="menu-icon"></div>
             </div>
           </button>
         </nav>
-        
+
         <div className="home">
-          <div className="container">
-            
-          </div>
+          <div className="container"></div>
         </div>
       </div>
-  </header>
-  
-  )
-}
+    </header>
+  );
+};
 
-export default navigation
+export default navigation;
