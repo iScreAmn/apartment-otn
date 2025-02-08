08d0a772cbb7ec54d04f3f6bdd2fbe0229b5615e
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-08 17:11:29 +0400

    Updated react icons at services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,51 +1,69 @@
-import "./Services.css"
+import "./Services.css";
+import { FaWifi, FaTv, FaPlane } from "react-icons/fa";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaSquareParking } from "react-icons/fa6";
+import { FaMountainSun } from "react-icons/fa6";
 
 const Services = () => {
   return (
     <section className="services">
       <div className="container">
-        <h2 className="services-title">В нашем доме <span>есть:</span></h2>
+        <h2 className="services-title">
+          В нашем доме <span>есть:</span>
+        </h2>
         <div className="services__wrapper">
           <div className="card">
-            <i className="fa-solid fa-wifi"></i>
+            <FaWifi />
             <h2 className="card__title">Быстрый Интернет</h2>
-            <p className="card_subtitle">На территории каждого дома, есть интернет и быстрый доступ к WiFi с различными
-              настройками</p>
+            <p className="card_subtitle">
+              На территории каждого дома, есть интернет и быстрый доступ к WiFi
+              с различными настройками
+            </p>
           </div>
           <div className="card">
-            <i className="fa-solid fa-tv"></i>
+            <FaTv />
             <h2 className="card__title">HD Телевиденье</h2>
-            <p className="card_subtitle">На территории каждого дома, есть HD телевизор и доступ к фильмам с различными
-              языками</p>
+            <p className="card_subtitle">
+              На территории каждого дома, есть HD телевизор и доступ к фильмам с
+              различными языками
+            </p>
           </div>
           <div className="card">
-            <i className="fa-solid fa-plane"></i>
+            <FaPlane />
             <h2 className="card__title">Трансфер с аэропорта</h2>
-            <p className="card_subtitle">Мы можем организовать трансфер с любого аэропорта страны в удобное для вас время
+            <p className="card_subtitle">
+              Мы можем организовать трансфер с любого аэропорта страны в удобное
+              для вас время
             </p>
             <button className="card__btn">подроднее</button>
           </div>
           <div className="card">
-            <i className="fa-solid fa-location-dot"></i>
+            <IoLocationOutline />
             <h2 className="card__title">Идеальная локация</h2>
-            <p className="card_subtitle">Апартаменты расположены в самом центре старого города, все топовые локации рядом
+            <p className="card_subtitle">
+              Апартаменты расположены в самом центре старого города, все топовые
+              локации рядом
             </p>
             <button className="card__btn">подроднее</button>
           </div>
           <div className="card">
-            <i className="fa-solid fa-square-parking"></i>
+            <FaSquareParking />
             <h2 className="card__title">Паркинг</h2>
-            <p className="card_subtitle">На территории дома, есть паркинг и быстрый доступ к машине</p>
+            <p className="card_subtitle">
+              На территории дома, есть паркинг и быстрый доступ к машине
+            </p>
           </div>
           <div className="card">
-            <i className="fa-solid fa-mountain-sun"></i>
+            <FaMountainSun />
             <h2 className="card__title">Туры</h2>
-            <p className="card_subtitle">Мы можем предложить вам интересные туры на время вашего проживания</p>
+            <p className="card_subtitle">
+              Мы можем предложить вам интересные туры на время вашего проживания
+            </p>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
