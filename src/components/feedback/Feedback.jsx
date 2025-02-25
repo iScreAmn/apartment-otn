3a6082b06f05149a32bf44d26b10923e08a5fbd1
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-26 01:43:28 +0400

    Initialised feedback section with scroll function

---
+++ src/components/feedback/Feedback.jsx
@@ -0,0 +1,61 @@
+import "./Feedback.css";
+import { avatar1, avatar2, avatar3 } from "../../assets/images/index";
+import { Carousel } from "nuka-carousel";
+
+
+const Feedback = () => {
+  
+  return (
+    <section className="feedback">
+      <div className="container">
+        <h2 className="services-title">
+          Почему <span>нас выбирают</span>
+        </h2>
+          <Carousel showDots showArrows scrollDistance="slide" wrapMode="wrap">
+                <div className="feedback__card">
+                  <img src={avatar1} alt="" className="feedback__img" />
+                  <p className="feedback__subtitle">
+                    Очень повезло оказаться именно в эти апартаменты, близость с
+                    центром очень радует, рядом есть много магазинов
+                  </p>
+                  <h2 className="feedback__name">Дима</h2>
+                </div>
+                <div className="feedback__card">
+                  <img src={avatar2} alt="" className="feedback__img" />
+                  <p className="feedback__subtitle">
+                    Была рада побывать в этих апартаментах, кафешки рядом, пешком
+                    можно дойти до всех достопримечательностей
+                  </p>
+                  <h2 className="feedback__name">Лена</h2>
+                </div>
+                <div className="feedback__card">
+                  <img src={avatar3} alt="" className="feedback__img" />
+                  <p className="feedback__subtitle">
+                    Круто попасть в такие апартаменты, вокруг много мест куда можно
+                    сходить поужинать и насладиться видом
+                  </p>
+                  <h2 className="feedback__name">Миша</h2>
+                </div>
+                <div className="feedback__card">
+                  <img src={avatar3} alt="" className="feedback__img" />
+                  <p className="feedback__subtitle">
+                    Круто попасть в такие апартаменты, вокруг много мест куда можно
+                    сходить поужинать и насладиться видом
+                  </p>
+                  <h2 className="feedback__name">Миша</h2>
+                </div>
+                <div className="feedback__card">
+                  <img src={avatar3} alt="" className="feedback__img" />
+                  <p className="feedback__subtitle">
+                    Круто попасть в такие апартаменты, вокруг много мест куда можно
+                    сходить поужинать и насладиться видом
+                  </p>
+                  <h2 className="feedback__name">Миша</h2>
+                </div>
+          </Carousel>
+      </div>
+    </section>
+  );
+};
+
+export default Feedback;
