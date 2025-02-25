4af4baef9a676ce399c7f8ec632b60b71d97d840
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-26 01:49:38 +0400

    Add modal-window on btn click

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Home.css";
 import { FaVideo } from "react-icons/fa";
 import { RemoveScroll } from "react-remove-scroll";
+import { motion } from "motion/react";
+import { FaCalendarAlt } from "react-icons/fa";
+import { slideInVariants } from "../../../utils/animation";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isVideoOpen, setIsVideoOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  
+
   const videoId = "riZAVELEHpk"; // Замените на нужный YouTube ID
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [guests, setGuests] = useState(1);
 
-  // Функция для плавного скролла
-  const scrollToBooking = (e) => {
-    e.preventDefault(); // Отменяем стандартное поведение ссылки
-
-    const targetId = e.currentTarget.getAttribute('href'); // Получаем ID целевого элемента
-    const targetElement = document.querySelector(targetId); // Находим целевой элемент
-
-    if (targetElement) {
-      // Плавный скролл к целевому элементу
-      targetElement.scrollIntoView({
-        behavior: 'smooth', // Плавная прокрутка
-        block: 'start'      // Выравнивание по верху элемента
-      });
-    }
+  const handleCheckAvailability = () => {
+    // Логика для проверки наличия
+    console.log("Check-In:", checkIn);
+    console.log("Check-Out:", checkOut);
+    console.log("Guests:", guests);
+    alert("Проверка наличия...");
   };
 
   return (
@@ -32,15 +35,19 @@ const Home = () => {
             Old Tbilisi <span>Narikala</span>
           </h2>
           <div className="home__btns">
-            <a href="#booking" className="home__btn" onClick={scrollToBooking}>
-              Забронировать
-            </a>
-            <button 
-              className="home__video-btn" 
+            <a
+              href="#!"
+              className="home__btn"
               onClick={() => setIsModalOpen(true)}
             >
+              Забронировать
+            </a>
+            <button
+              className="home__video-btn"
+              onClick={() => setIsVideoOpen(true)}
+            >
               Смотреть дом
-              <FaVideo/>
+              <FaVideo />
             </button>
           </div>
         </div>
@@ -58,13 +65,23 @@ const Home = () => {
       </div>
 
       {/* Модальное окно с видео */}
-      {isModalOpen && (
+      {isVideoOpen && (
         <RemoveScroll>
-          <div className="home-modal-overlay" onClick={() => setIsModalOpen(false)}>
-            <div className="home-modal-content" onClick={(e) => e.stopPropagation()}>
-              <button className="home-modal-close" onClick={() => setIsModalOpen(false)}>×</button>
+          <div
+            className="home-modal-overlay"
+            onClick={() => setIsVideoOpen(false)}
+          >
+            <div
+              className="home-modal-content"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <button
+                className="home-modal-close"
+                onClick={() => setIsVideoOpen(false)}
+              >
+                ×
+              </button>
               <iframe
-                
                 src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
                 title="YouTube video"
                 frameBorder="0"
@@ -75,6 +92,107 @@ const Home = () => {
           </div>
         </RemoveScroll>
       )}
+
+      {/* Модальное окно с бронью */}
+      {isModalOpen && (
+        <RemoveScroll>
+          <div
+            className="home-modal-overlay"
+            onClick={() => setIsModalOpen(false)}
+          >
+            <div
+              className="home-modal-content"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <button
+                className="home-modal-close"
+                onClick={() => setIsModalOpen(false)}
+              >
+                ×
+              </button>
+              <section className="booking-section" id="booking">
+                <h2 className="services-title">
+                  Забронируй <span>Онлайн</span>
+                </h2>
+                <div className="booking-form">
+                  <div className="form-group">
+                    <label htmlFor="check-in">Заезд:</label>
+                    <motion.div
+                      className="input-with-icon"
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: false, amount: 0.5 }}
+                      custom={1}
+                      variants={slideInVariants("top", 0.7, 50, true)}
+                    >
+                      <input
+                        type="date"
+                        id="check-in"
+                        value={checkIn}
+                        onChange={(e) => setCheckIn(e.target.value)}
+                      />
+                      <FaCalendarAlt className="calendar-icon" />
+                    </motion.div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="check-out">Выезд:</label>
+                    <motion.div
+                      className="input-with-icon"
+                      initial="hidden"
+                      whileInView="visible"
+                      viewport={{ once: false, amount: 0.5 }}
+                      custom={2}
+                      variants={slideInVariants("top", 0.7, 50, true)}
+                    >
+                      <input
+                        type="date"
+                        id="check-out"
+                        value={checkOut}
+                        onChange={(e) => setCheckOut(e.target.value)}
+                      />
+                      <FaCalendarAlt className="calendar-icon" />
+                    </motion.div>
+                  </div>
+                  <motion.div
+                    className="form-group"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: false, amount: 0.5 }}
+                    custom={3}
+                    variants={slideInVariants("top", 0.7, 50, true)}
+                  >
+                    <label htmlFor="guests">Гости:</label>
+                    <input
+                      type="number"
+                      id="guests"
+                      min="1"
+                      value={guests}
+                      onChange={(e) => setGuests(Number(e.target.value))}
+                    />
+                  </motion.div>
+                  <motion.div
+                    className="order__wrapper"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: false, amount: 0.5 }}
+                    custom={4}
+                    variants={slideInVariants("top", 0.7, 50, true)}
+                  >
+                    <button
+                      className="order__btn"
+                      onClick={handleCheckAvailability}
+                    >
+                      Показать наличие
+                    </button>
+                  </motion.div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </RemoveScroll>
+      )}
+
+      
     </>
   );
 };
