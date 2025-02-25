797ee2147427b5912e9692655cfbb615b04e7e0f
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-26 01:41:18 +0400

    Add avatars to data

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,13 @@
 import logo from "./logos/main-logo.webp"
 
+import avatar1 from "./avatars/avatar-1.webp"
+import avatar2 from "./avatars/avatar-2.webp"
+import avatar3 from "./avatars/avatar-3.webp"
+
 
 export {
   logo,
+  avatar1,
+  avatar2,
+  avatar3,
 }
