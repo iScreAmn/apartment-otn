7d51cf65ce149b3aee09ee4332b9e548cfa68774
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-04-04 21:46:32 +0400

    Add circular-text to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,27 @@
-import { logo } from "../../assets/images";
+import Logo from "../logo/Logo";
 import "./Footer.css";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer__wrapper">
           <div className="nav__logo">
-            <a href="#!">
-              <img src={logo} alt="Main Image" />
-            </a>
+            <Logo
+              text="OLD•TBILISI•NARIKALA•"
+              onHover="speedUp"
+              spinDuration={20}
+              className="custom-class"
+            />
           </div>
-          <p className="footer__copyright">OLD TBILISI <span className="footer-span">NARIKALA</span>, All rights reserved.</p>
-          <p className="footer__year">© {currentYear}</p>
-          
+          <div className="footer__info">
+            <p className="footer__copyright">
+              OLD TBILISI <span className="footer-span">NARIKALA</span>, All
+              rights reserved.
+            </p>
+            <p className="footer__year">© {currentYear}</p>
+          </div>
         </div>
       </div>
     </footer>
