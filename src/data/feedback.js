7b441e6e90fb310b52ca3fd14b70bfd76cd7929c
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-05-18 15:02:07 +0400

    Included Feedback data resources

---
+++ src/data/feedback.js
@@ -0,0 +1,41 @@
+import { avatar1, avatar2, avatar3, avatar4, avatar5, avatar6 } from "../assets/images";
+
+export const feedback = [
+  {
+    titleKey: "feedback.review1.title",
+    textKey: "feedback.review1.text",
+    nameKey: "feedback.review1.name",
+    img: avatar1,
+  },
+  {
+    titleKey: "feedback.review2.title",
+    textKey: "feedback.review2.text",
+    nameKey: "feedback.review2.name",
+    img: avatar2,
+  },
+  {
+    titleKey: "feedback.review3.title",
+    textKey: "feedback.review3.text",
+    nameKey: "feedback.review3.name",
+    img: avatar3,
+  },
+  {
+    titleKey: "feedback.review4.title",
+    textKey: "feedback.review4.text",
+    nameKey: "feedback.review4.name",
+    img: avatar4,
+  },
+  {
+    titleKey: "feedback.review5.title",
+    textKey: "feedback.review5.text",
+    nameKey: "feedback.review5.name",
+    img: avatar5,
+  },
+  {
+    titleKey: "feedback.review6.title",
+    textKey: "feedback.review6.text",
+    nameKey: "feedback.review6.name",
+    img: avatar6,
+  },
+  
+];
